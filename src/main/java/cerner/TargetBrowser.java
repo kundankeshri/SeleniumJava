4804bc8b264326014cec30a0fd1da8ddc48a6fd1
C_Kumar, Kundan <C_Kumar.Kundan@cerner.com>
2018-09-10 15:35:37 +0530

    add all

--- src/main/java/cerner/TargetBrowser.java
+++ src/main/java/cerner/TargetBrowser.java
@@ -52,6 +52,7 @@ public class TargetBrowser {
 			}
 
 		   if(chosen == BROWSER.CHROME && checkAccessibility == "ACCESSIBILITY"){
+				 System.out.println("InsideChosen"+chosen);
 				System.setProperty("webdriver.chrome.driver",exeLocation);
 				ChromeOptions options = new ChromeOptions();
 				options.addArguments("load-extension=C:/KeshriKundanAccess/extension/a11yChromeExtn");
